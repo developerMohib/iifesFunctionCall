a13fde03776d0c36e68d4cde9cdaefb1e47785c2
developerMohib <92154638+developerMohib@users.noreply.github.com>
2023-05-28 17:03:25 +0600

    iifes Function call

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// Immediately Invocable Function Expression ( IIFEs )
+
+// make a function for two number summation 
+
+(function addition(num1, num2) {
+  let result = num1 + num2;
+  document.write(`immediately function call two number sum = ` + result)
+})(44, 7)
